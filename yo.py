a589b998450bc5efe8cded9d2dcdf3c141eda6d0
shreyu21 <shreyajaiswal131205@gmail.com>
2026-06-01 15:38:44 +0530

    Update yo.py

--- yo.py
+++ yo.py
@@ -307,7 +307,7 @@ run = st.checkbox("Start Speed Detection")
 
 FRAME_WINDOW = st.image([])
 
-camera = cv2.VideoCapture(0)
+camera = cv2.VideoCapture(1)
 
 # =====================================================
 # VARIABLES
@@ -334,7 +334,7 @@ while run:
     # YOLO Detection
     results = model(frame)
 
-    annotated_frame = results[0].plot()
+    annotated_frame = results[1].plot()
 
     # ============================================
     # DETECT VEHICLES
@@ -644,7 +644,7 @@ run = st.checkbox("▶ Start Speed Monitoring")
 
 FRAME_WINDOW = st.image([])
 
-camera = cv2.VideoCapture(0)
+camera = cv2.VideoCapture(1)
 
 # =====================================================
 # VARIABLES
@@ -680,7 +680,7 @@ while run:
     # YOLO Detection
     results = model(frame)
 
-    annotated_frame = results[0].plot()
+    annotated_frame = results[1].plot()
 
     # =================================================
     # DETECT VEHICLES
